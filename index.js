90646874d25a9ed0dee86327b0d1f2d4e52a6e9e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-14 09:55:01 +0300

    add js and html files

---
+++ index.js
@@ -0,0 +1,4 @@
+
+// Object   =    A collection of related properties and/or methods
+//                Can represent real world objects(people, products, places)
+//                Object = {key:value}
